bf66f069315c02c7653602dff7481c9a29e37cd0
janekim0730 <janekim0730@gmail.com>
2016-05-29 06:35:09 -0700

    template for search result page is added.

--- themes/inhabitent/search.php
+++ themes/inhabitent/search.php
@@ -11,9 +11,9 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html( 'Search Results for: %s' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+<?php echo "Hi~~~~"?>
+			<header class="search-header">
+				<h1 class="search-title"><?php printf( esc_html( 'Search Results for: %s' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
 			</header><!-- .page-header -->
 
 			<?php /* Start the Loop */ ?>
@@ -32,7 +32,8 @@ get_header(); ?>
 		<?php endif; ?>
 
 		</main><!-- #main -->
-	</section><!-- #primary -->
 
 <?php get_sidebar(); ?>
+</section><!-- #primary -->
+
 <?php get_footer(); ?>
